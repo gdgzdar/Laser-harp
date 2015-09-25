a39b3175bb636e02969ad571d57e70991fb5ecc2
yagarea <j.cerny.zdar@gmail.com>
2015-09-25 20:46:02 +0200

    Create Harp_firmware.ino

---
+++ Harp_firmware.ino
@@ -0,0 +1,79 @@
+//První oktáva
+int laser1 = 26;
+int laser2 = 27;
+int laser3 = 28;
+int laser4 = 29;
+int laser5 = 30;
+int laser6 = 31;
+int laser7 = 32;
+int laser8 = 33;
+int oktava1[8] = {laser1, laser2, laser3, laser4, laser5, laser6, laser7, laser8};
+
+//Druhá oktáva
+int laser9 = 34;
+int laser10 = 35;
+int laser11 = 36;
+int laser12 = 37;
+int laser13 = 38;
+int laser14 = 39;
+int laser15 = 40;
+int laser16 = 41;
+int oktava2[8] = (laser9, laser10, laser11, laser12, laser13, laser14, laser15, laser16);
+
+//druhá oktáva
+int laser9 = 34;
+int laser10 = 35;
+int laser11 = 36;
+int laser12 = 37;
+int laser13 = 38;
+int laser14 = 39;
+int laser15 = 40;
+int laser16 = 41;
+int oktava2[8] = {laser9, laser10, laser11, laser12, laser13, laser14, laser15, laser16};
+
+//Třetí oktáva
+int laser17 = 42;
+int laser18 = 43;
+int laser19 = 44;
+int laser20 = 45;
+int laser21 = 46;
+int laser22 = 47;
+int laser23 = 48;
+int laser24 = 49;
+int oktava3[8] = {laser17, laser18, laser19, laser20, laser21, laser22, laser23, laser24};
+
+
+void setup() {                
+  Serial.begin(9600);
+  pinMode(laser1, OUTPUT);
+  pinMode(laser2, OUTPUT);
+  pinMode(laser3, OUTPUT);
+  pinMode(laser4, OUTPUT);
+  pinMode(laser5, OUTPUT);
+  pinMode(laser6, OUTPUT);
+  pinMode(laser7, OUTPUT);
+  pinMode(laser8, OUTPUT);
+  
+  pinMode(laser9, OUTPUT);  
+  pinMode(laser10, OUTPUT);
+  pinMode(laser11, OUTPUT);
+  pinMode(laser12, OUTPUT);
+  pinMode(laser13, OUTPUT);
+  pinMode(laser14, OUTPUT);
+  pinMode(laser15, OUTPUT);
+  pinMode(laser1, OUTPUT);
+  
+  pinMode(laser17, OUTPUT);
+  pinMode(laser18, OUTPUT);
+  pinMode(laser19, OUTPUT);
+  pinMode(laser20, OUTPUT);
+  pinMode(laser21, OUTPUT);
+  pinMode(laser22, OUTPUT);
+  pinMode(laser23, OUTPUT);  
+
+  
+}
+
+void loop() {
+  
+}
