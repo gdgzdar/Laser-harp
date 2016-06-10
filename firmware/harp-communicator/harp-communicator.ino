472fe3ec91c4ef80507d9e946b42c35fb478c730
yagarea <j.cerny.zdar@gmail.com>
2016-06-10 17:36:13 +0200

    fix critical issue

--- firmware/harp-communicator/harp-communicator.ino
+++ firmware/harp-communicator/harp-communicator.ino
@@ -4,21 +4,23 @@ static const byte NUMBER_OF_LASERS = 26;
 Laser* lasers[NUMBER_OF_LASERS];
 
 void setup() {
-  for (int i = 0; i = NUMBER_OF_LASERS; i++) {
+  Serial.begin(9600);
+  for (int i = 0; i < NUMBER_OF_LASERS; i++) {
     // set pins for lasers and sensors
-    int sensorPin = i + 28; // this index may be wrong
-    int laserPin = i + 2; // this one too
+    int sensorPin = i + 29; // this index may be wrong
+    int laserPin = i + 3; // this one too
+    Serial.println(laserPin);
     pinMode(laserPin, OUTPUT);    
     pinMode(sensorPin, INPUT);
     digitalWrite(laserPin, HIGH);
     lasers[i] = new Laser(true, false, sensorPin, laserPin);
   }
-  Serial.begin(9600);
 }
 
 void loop() {
-  for(int i = 0 ; i = NUMBER_OF_LASERS; i++) {
+  for(int i = 0; i < NUMBER_OF_LASERS; i++) {
     lasers[i]->isCrossed = digitalRead(lasers[i]->sensorPin) != HIGH;
     Serial.print(lasers[i]->isCrossed);
   }
+  Serial.println();
 }
