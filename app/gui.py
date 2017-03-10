4245123d9082fd2072195e6156ccc8155f67775b
Vaclav Pavlicek <vaclav@pavlicek.cz>
2017-03-10 18:10:28 +0100

    add menu template without colors

---
+++ app/gui.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class InstrumentsList(Widget):
+    pass
+
+
+class HarpApp(App):
+    def build(self):
+        return InstrumentsList()
+
+
+if __name__ == '__main__':
+    HarpApp().run()
